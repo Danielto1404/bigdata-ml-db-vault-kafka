2851a7d65769daa5c1fa1a1a71179c09e306168c
danielto1404 <daniilkorolev00@gmail.com>
2023-06-12 18:36:08 +0300

    store kafka credentials to vault

--- src/vault.py
+++ src/vault.py
@@ -1,8 +1,8 @@
+import argparse
 import dataclasses
 import logging
 
 import hvac
-import argparse
 
 HASHICORP_VAULT_HOST = "vault"
 HASHICORP_VAULT_PORT = 8200
@@ -15,8 +15,15 @@ class PostgresCredentials:
     dbname: str
 
 
+@dataclasses.dataclass
+class KafkaCredentials:
+    host: str
+    port: str
+
+
 class HashicorpVault:
     POSTGRES_CREDENTIALS_PATH = "postgres/credentials"
+    KAFKA_CREDENTIALS_PATH = "kafka/credentials"
 
     def __init__(self, token: str):
         url = f"http://{HASHICORP_VAULT_HOST}:{HASHICORP_VAULT_PORT}"
@@ -34,6 +41,18 @@ class HashicorpVault:
             secret=dict(user=user, password=password, dbname=dbname)
         )
 
+    def read_kafka_credentials(self) -> KafkaCredentials:
+        secrets = self.client.secrets.kv.v2.read_secret_version(
+            path=self.KAFKA_CREDENTIALS_PATH
+        )
+        return KafkaCredentials(**secrets["data"]["data"])
+
+    def store_kafka_credentials(self, host: str, port: str):
+        self.client.secrets.kv.v2.create_or_update_secret(
+            path=self.KAFKA_CREDENTIALS_PATH,
+            secret=dict(host=host, port=port)
+        )
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Hashicorp Vault CLI")
@@ -41,14 +60,22 @@ if __name__ == "__main__":
     parser.add_argument("--postgres-user", required=True)
     parser.add_argument("--postgres-password", required=True)
     parser.add_argument("--postgres-dbname", required=True)
+    parser.add_argument("--kafka-host", required=True)
+    parser.add_argument("--kafka-port", required=True)
 
     args = parser.parse_args()
 
     vault = HashicorpVault(token=args.token)
+
     vault.store_postgres_credentials(
         user=args.postgres_user,
         password=args.postgres_password,
         dbname=args.postgres_dbname
+    )
+
+    vault.store_kafka_credentials(
+        host=args.kafka_host,
+        port=args.kafka_port
     )
 
     logging.info("Credentials stored")
